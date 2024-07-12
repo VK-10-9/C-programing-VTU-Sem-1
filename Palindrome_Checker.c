ce207914f824362bd3a9cd5bb6074c79bf406c3c
VK-10-9 <vishwanathkoliwad@gmail.com>
2024-07-12 13:51:28 +0530

    Update Palindrome_Checker.c
    This repository contains a simple C program to check if a given number is a palindrome. The program reads a positive integer from the user, reverses the number, and then checks if the original number is equal to the reversed number. If they are equal, the number is a palindrome; otherwise, it is not. This repository is useful for beginners learning C programming and basic algorithms.

--- Palindrome_Checker.c
+++ Palindrome_Checker.c
@@ -1,32 +1,38 @@
-//Palindrome Checker
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int num, revnum = 0, remainder, ognum;
 
-    // Prompt the user to enter a number
+    // Prompt user to enter a number
     printf("Enter the number to check if it is a palindrome or not: ");
     scanf("%d", &num);
-
-    // Store the original number for comparison later
+    
+    // Display the entered number
+    printf("Your entered number is: %d", num);
+    
+    // Store the original number
     ognum = num;
-
+    
+    // Check if the entered number is negative
+    if (num < 0) {
+        printf("\nThe entered number %d is invalid ", num);
+        return 0;
+    }
+    
     // Reverse the number
-    while (num != 0)
-    {
-        remainder = num % 10;            // Get the last digit of num
-        revnum = revnum * 10 + remainder; // Append the last digit to revnum
-        num /= 10;                       // Remove the last digit from num
+    while (num != 0) { 
+        remainder = num % 10;             // Get the last digit
+        revnum = revnum * 10 + remainder; // Append it to the reversed number
+        num /= 10;                        // Remove the last digit
     }
 
+    // Display the reversed number
+    printf("\nYour reversed number is: %d", revnum);
+    
     // Check if the original number is equal to the reversed number
-    if (ognum == revnum)
-    {
-        printf("The given number is a palindrome\n");
-    } 
-    else 
-    {
+    if (ognum == revnum) {
+        printf("\nThe given number is a palindrome\n");
+    } else {
         printf("The given number is not a palindrome\n");
     }
 
